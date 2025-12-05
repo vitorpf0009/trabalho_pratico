c0f26999b4f054b47805b1ba0340b3be78592fc9
vitorpf0009 <victorsantos.pf009@academico.ifsul.edu.br>
2025-12-05 10:03:41 -0300

    email

--- routes/rotasUsuario.js
+++ routes/rotasUsuario.js
@@ -6,12 +6,12 @@ const {verificaJWT} = require('../controllers/segurancaController');
 // Rota pública para criação de usuários
 router.post('/', UsuarioController.create);
 router.get('/', UsuarioController.findAll);
-router.put('/:id', UsuarioController.update);
-router.delete('/:id', UsuarioController.delete);
+router.put('/:email', UsuarioController.update);
+router.delete('/:email', UsuarioController.delete);
 
 // Rotas protegidas
 router.get('/', verificaJWT, UsuarioController.findAll);
-router.put('/:id', verificaJWT, UsuarioController.update);
-router.delete('/:id', verificaJWT, UsuarioController.delete);
+router.put('/:email', verificaJWT, UsuarioController.update);
+router.delete('/:email', verificaJWT, UsuarioController.delete);
 
 module.exports = router;
